99338f892ffd7904d9efd40c4d7f3ce28a34b9bf
MonicaDeBona <debona.monica@outlook.it>
2022-12-07 15:44:33 +0100

    Create elements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 /*
 Descrizione
 Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
 Milestone 1 -
-Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post nel nostro feed, (rimuovendo il post di prova dall'html).
+    Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post nel nostro feed, (rimuovendo il post di prova dall'html).
+
 Milestone 2 -
-Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+    Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 BONUS
 1 : Formattare le date in formato italiano (gg/mm/aaaa)
@@ -74,3 +76,63 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// ``
+
+
+const divElement = document.getElementById('container');
+console.log(divElement);
+
+posts.forEach(element => {
+    const postContainer = document.createElement('div');
+    postContainer.classList.add('post');
+    divElement.append(postContainer);
+    
+    const postHeader = document.createElement('div');
+    postHeader.classList.add('post_header');
+    postContainer.append(postHeader);
+
+    const postMeta = document.createElement('div');
+    postMeta.classList.add('post-meta');
+    postHeader.append(postMeta);
+
+    const postIcon = document.createElement('div');
+    postIcon.classList.add('post-meta__icon');
+    postMeta.append(postIcon);
+
+    const postData = document.createElement('div');
+    postData.classList.add('post-meta__data');
+    postMeta.append(postData);
+
+    const postMetaAuthor = document.createElement('div');
+    postMetaAuthor.classList.add('post-meta__author');
+    postData.append(postMetaAuthor);
+
+    const postMetaTime = document.createElement('div');
+    postMetaTime.classList.add('post-meta__time');
+    postData.append(postMetaTime);
+
+    const postText = document.createElement('div');
+    postText.classList.add('post__text');
+    postContainer.append(postText);
+
+    const postImage = document.createElement('div');
+    postImage.classList.add('post__image');
+    postContainer.append(postImage);
+
+    const postFooter = document.createElement('div');
+    postFooter.classList.add('post__footer');
+    postContainer.append(postFooter);
+
+    const postLike = document.createElement('div');
+    postLike.classList.add('likes', 'js-likes');
+    postFooter.append(postLike);
+
+    const postLikeCta = document.createElement('div');
+    postLikeCta.classList.add('likes__cta');
+    postLike.append(postLikeCta);
+
+    const postLikeCounter = document.createElement('div');
+    postLikeCounter.classList.add('likes__counter');
+    postLike.append(postLikeCounter);
+});
